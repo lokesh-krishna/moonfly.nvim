340a1ab6b15fe0fe7150575cc2853820b75ddf74
Lokesh Krishna <lokesh@low-key.me>
2021-08-16 12:14:41 +0530

    custom lualine

--- lua/lualine/themes/moonfly.lua
+++ lua/lualine/themes/moonfly.lua
@@ -5,37 +5,40 @@
 
 -- LuaFormatter off
 local colors = {
-  color3   = '#303030',
-  color6   = '#9e9e9e',
-  color7   = '#80a0ff',
-  color8   = '#ae81ff',
-  color0   = '#1c1c1c',
-  color1   = '#ff5189',
-  color2   = '#c6c6c6',
+	red	= '#ff5454',
+	green	= '#8cc85f',
+	yellow	= '#e3c78a',
+	blue	= '#80a0ff',
+	purple	= '#d183e8',
+	cyan	= '#79dac8',
+	black	= '#080808',
+	white	= '#b2b2b2',
+	bright_red	= '#ff5189',
+	grey	= '#303030',
 }
 
 -- LuaFormatter on
 return {
   replace = {
-    a = {fg = colors.color0, bg = colors.color1, gui = 'bold'},
-    b = {fg = colors.color2, bg = colors.color3}
+    a = {fg = colors.black, bg = colors.bright_red, gui = 'bold'},
+    b = {fg = colors.white, bg = colors.grey}
   },
   inactive = {
-    a = {fg = colors.color6, bg = colors.color3, gui = 'bold'},
-    b = {fg = colors.color6, bg = colors.color3},
-    c = {fg = colors.color6, bg = colors.color3}
+    a = {fg = colors.white, bg = colors.grey, gui = 'bold'},
+    b = {fg = colors.white, bg = colors.grey},
+    c = {fg = colors.white, bg = colors.grey}
   },
   normal = {
-    a = {fg = colors.color0, bg = colors.color7, gui = 'bold'},
-    b = {fg = colors.color2, bg = colors.color3},
-    c = {fg = colors.color2, bg = colors.color3}
+    a = {fg = colors.black, bg = colors.purple, gui = 'bold'},
+    b = {fg = colors.white, bg = colors.grey},
+    c = {fg = colors.white, bg = colors.grey}
   },
   visual = {
-    a = {fg = colors.color0, bg = colors.color8, gui = 'bold'},
-    b = {fg = colors.color2, bg = colors.color3}
+    a = {fg = colors.black, bg = colors.cyan, gui = 'bold'},
+    b = {fg = colors.white, bg = colors.grey}
   },
   insert = {
-    a = {fg = colors.color0, bg = colors.color2, gui = 'bold'},
-    b = {fg = colors.color2, bg = colors.color3}
+    a = {fg = colors.black, bg = colors.blue, gui = 'bold'},
+    b = {fg = colors.white, bg = colors.grey}
   }
 }
